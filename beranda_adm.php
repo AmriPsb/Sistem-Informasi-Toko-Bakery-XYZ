7b0adc2b2eac2ebf9fd0f2fe536dae9d94b1ad24
AmriPsb <77764416+AmriPsb@users.noreply.github.com>
2021-01-21 10:02:25 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,38 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            <div class="alert alert-info">
+                Selamat datang kembali <strong><?=$_SESSION['nama']?></strong>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Halaman Penjualan Roti Toko XYZ</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img/m.png"></img></p>
+                          <p class="label-title" align="center"><strong>TOKO ROTI XYZ</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman Penjualan Roti Toko XYZ.<br>
+                            Melalui halaman ini dapat dilakukan pengelolaan data roti yang ingin dibeli, <br>
+                            dan juga pengelolaan laporan hasil penjualan roti.<br><br>
+                            Akses menu Master Data pada bagian atas sistem untuk pengelolaan input data roti dan data roti.<br>
+                            Untuk mengelola laporan, dapat dilakukan dengan mengakses menu report pada bagian atas sistem.<br>
+                            Akses menu User untuk mengelola informasi tentang user yang login.<br><br><br>
+
+                          </p>
+
+                         </thead
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
